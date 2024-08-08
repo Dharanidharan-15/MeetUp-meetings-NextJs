b7a4209adc67cf3c7893fd88fcb2e393ad094c28
Dharanidharan-15 <dharanidharan253@gmail.com>
2024-08-08 23:45:24 +0530

    updated changes on time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,22 @@
+"use client";
+
 import MeetingTypeList from "@/components/MeetingTypeList";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [currentTime, setCurrentTime] = useState("");
   const now = new Date();
-  const time = now.toLocaleTimeString("en-In", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+
   const date = new Intl.DateTimeFormat("en-In", {
     dateStyle: "full",
   }).format(now);
+  useEffect(() => {
+    const time = now.toLocaleTimeString("en-In", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    setCurrentTime(time);
+  }, [currentTime]);
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
@@ -17,7 +25,9 @@ const Home = () => {
             Upcoming Meeting at: 12:30 PM
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">
+              {currentTime}
+            </h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
